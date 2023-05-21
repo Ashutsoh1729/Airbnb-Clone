307dd365a374387938c02a9847974b3d51695b5d
ashutoshhota <youremail>
2023-05-21 18:37:03 +0530

    Now i have finished the favorite functionalities in which i build a hook, write the api post and delete request. Now lets build the indivisula listing view in which the customer can see every listings indivisually.

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,84 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from '../../../libs/prismadb'
+
+
+interface IParms{
+    listingId?:string,
+}
+
+export async function POST(response: Response , { params }: {params : IParms}) {
+    const currentUser = await getCurrentUser();
+    
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+    
+    const { listingId } = params;
+    
+    if (!listingId || typeof listingId !== 'string') {
+        throw new Error("Invalid Id, location: api/favorite//route.ts")
+    }
+    
+    let favoriteIds = [...(currentUser.favoriteIds || [])];
+    
+    favoriteIds.push(listingId);
+    
+    const user = await prisma.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data: {
+            favoriteIds: favoriteIds,
+        }
+    })
+    
+    return NextResponse.json(user);
+}
+
+// Here we can create a delete function 
+
+export async function DELETE(res: Response, { params }: { params: IParms }) {
+    
+    const currentUser = await getCurrentUser();
+    
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+
+    const { listingId } = params;
+
+    
+    if (!listingId || typeof listingId !== 'string') {
+        throw new Error("Invalid Id, location: api/favorite//route.ts")
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+    favoriteIds = favoriteIds.filter((id)=>{id!==listingId})
+
+    const user = await prisma.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data: {
+            favoriteIds: favoriteIds,
+        }
+    });
+
+    return NextResponse.json(user);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -3,11 +3,12 @@
 import React from 'react'
 import { SafeUser } from '../types'
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '../hooks/useFavorite';
 
 
 
 interface HeartButtonProps {
-    listingId?: string,
+    listingId: string,
     currentUser?: SafeUser | null,
 }
 
@@ -16,8 +17,12 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,currentUser
 }) => {
 
-    const hasFavorited = false;
-    const toggleFavorite = () => { };    
+    // const hasFavorited = false;
+    // const toggleFavorite = () => { };
+
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId, currentUser
+    });
 
 
   return (
